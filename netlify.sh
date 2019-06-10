9d2b926a5fbe2cf2ef14211ba2483d7ea49df8dd
Jimmy Sawczuk <me@jimmysawczuk.com>
2019-06-09 21:30:21 -0400

    Deploy to Netlify automatically
    Deploying to Netlify automatically

---
+++ netlify.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+go get github.com/jimmysawczuk/tmpl
+go get github.com/jimmysawczuk/scm-status
+
+export PATH=$GOPATH/bin:$PATH
+
+scm-status -out=REVISION.json
+MODE=production tmpl -o public/index.html -timestamp-assets=false tmpl/index.tmpl
+MODE=production tmpl -o public/resume/index.html -timestamp-assets=false tmpl/resume.tmpl
+
+npm run build
+cp -R img *.png *.xml *.ico REVISION.json manifest.json _redirects _headers public/
