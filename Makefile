bb0e4f8ae4bbabc07027f6ec8c7b183b90177f73
Jimmy Sawczuk <github@jimmysawczuk.com>
2020-01-18 15:02:47 -0500

    Rework resume, styles

--- Makefile
+++ Makefile
@@ -5,7 +5,8 @@ dev: clean
 	MODE=dev tmpl -o public/index.html tmpl/index.tmpl
 	MODE=dev tmpl -o public/resume/index.html tmpl/resume.tmpl
 	cp -R img *.png *.xml *.ico REVISION.json manifest.json browserconfig.xml public/
-	npm run build
+	yarn minify-html
+	yarn build
 
 clean:
 	rm -rf public
--- netlify.sh
+++ netlify.sh
@@ -10,6 +10,6 @@ export PATH=$GOPATH/bin:$PATH
 scm-status -out=REVISION.json
 MODE=production tmpl -o public/index.html -timestamp-assets=false tmpl/index.tmpl
 MODE=production tmpl -o public/resume/index.html -timestamp-assets=false tmpl/resume.tmpl
-
-npm run build
+yarn minify-html
+yarn build
 cp -R img *.png *.xml *.ico REVISION.json manifest.json _redirects _headers public/
