74df14324142f3daa77b08534c8301b7b894a18b
Jimmy Sawczuk <github@jimmysawczuk.com>
2020-01-18 15:30:43 -0500

    Resume updates, modernize build env (#8)

--- src/main.js
+++ src/main.js
@@ -3,22 +3,22 @@ import "../less/main.less"
 import { library, dom } from "@fortawesome/fontawesome-svg-core"
 import { faPaperPlane, faIdCard } from "@fortawesome/pro-duotone-svg-icons"
 import {
-	faFacebookF,
-	faTwitter,
-	faInstagram,
-	faLinkedinIn,
-	faGithub,
-	faTelegramPlane,
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+  faLinkedinIn,
+  faGithub,
+  faTelegramPlane,
 } from "@fortawesome/free-brands-svg-icons"
 
 library.add(
-	faPaperPlane,
-	faIdCard,
-	faFacebookF,
-	faTwitter,
-	faInstagram,
-	faLinkedinIn,
-	faGithub,
-	faTelegramPlane,
+  faPaperPlane,
+  faIdCard,
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+  faLinkedinIn,
+  faGithub,
+  faTelegramPlane,
 )
 dom.i2svg()
