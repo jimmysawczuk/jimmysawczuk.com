45fa07252d9df86a0830299f9af46fb7153c7a9d
Jimmy Sawczuk <me@jimmysawczuk.com>
2020-01-26 12:54:23 -0500

    Sitemap

--- Makefile
+++ Makefile
@@ -4,7 +4,8 @@ dev: clean
 	scm-status -out=REVISION.json
 	MODE=dev tmpl -o public/index.html tmpl/index.tmpl
 	MODE=dev tmpl -o public/resume/index.html tmpl/resume.tmpl
-	cp -R img *.png *.xml *.ico REVISION.json manifest.json browserconfig.xml public/
+	MODE=dev tmpl -o public/sitemap.xml tmpl/sitemap.xml.tmpl
+	cp -R img *.png *.xml *.ico REVISION.json manifest.json public/
 	yarn minify-html
 	yarn build
 
--- netlify.sh
+++ netlify.sh
@@ -10,6 +10,7 @@ export PATH=$GOPATH/bin:$PATH
 scm-status -out=REVISION.json
 MODE=production tmpl -o public/index.html -timestamp-assets=false tmpl/index.tmpl
 MODE=production tmpl -o public/resume/index.html -timestamp-assets=false tmpl/resume.tmpl
+MODE=production tmpl -o public/sitemap.xml tmpl/sitemap.xml.tmpl
 yarn minify-html
 yarn build
 cp -R img *.png *.xml *.ico REVISION.json manifest.json _redirects _headers public/
