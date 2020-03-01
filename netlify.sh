c7f0636cb66177e2e3a5aa001406262882be7e2f
Jimmy Sawczuk <github@jimmysawczuk.com>
2020-02-29 21:15:02 -0500

    Upgrade tmpl (#9)

--- netlify.sh
+++ netlify.sh
@@ -2,15 +2,14 @@
 
 set -e
 
-go get github.com/jimmysawczuk/tmpl
-go get github.com/jimmysawczuk/scm-status
+go get -u github.com/jimmysawczuk/tmpl
+go get -u github.com/jimmysawczuk/scm-status
 
 export PATH=$GOPATH/bin:$PATH
 
 scm-status -out=REVISION.json
-MODE=production tmpl -o public/index.html -timestamp-assets=false tmpl/index.tmpl
-MODE=production tmpl -o public/resume/index.html -timestamp-assets=false tmpl/resume.tmpl
-MODE=production tmpl -o public/sitemap.xml tmpl/sitemap.xml.tmpl
-yarn minify-html
+tmpl -o public/index.html -fmt html -min tmpl/index.tmpl
+tmpl -o public/resume/index.html -fmt html -min tmpl/resume.tmpl
+tmpl -o public/sitemap.xml tmpl/sitemap.xml.tmpl
 yarn build
 cp -R img *.png *.xml *.ico REVISION.json manifest.json _redirects _headers public/
