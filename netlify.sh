c604d955ef3957f1ebfe05c3f4d709ec9825b81e
Jimmy Sawczuk <github@jimmysawczuk.com>
2020-06-16 15:04:04 -0400

    Switching production to new tmpl

--- netlify.sh
+++ netlify.sh
@@ -8,8 +8,6 @@ go get -u github.com/jimmysawczuk/scm-status
 export PATH=$GOPATH/bin:$PATH
 
 scm-status -out=REVISION.json
-tmpl -o public/index.html -fmt html -min tmpl/index.tmpl
-tmpl -o public/resume/index.html -fmt html -min tmpl/resume.tmpl
-tmpl -o public/sitemap.xml tmpl/sitemap.xml.tmpl
+tmpl
 yarn build
 cp -R img *.png *.xml *.ico REVISION.json manifest.json _redirects _headers public/
