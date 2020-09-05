e5d20fa9cf99362d32521cfb987f3149ed65b5b2
Jimmy Sawczuk <github@jimmysawczuk.com>
2020-09-05 17:19:52 -0400

    Dark Mode 2.0 (#18)

--- netlify.sh
+++ netlify.sh
@@ -8,6 +8,7 @@ go get -u github.com/jimmysawczuk/scm-status
 export PATH=$GOPATH/bin:$PATH
 
 scm-status -out=REVISION.json
+yarn dark-mode
 tmpl
 yarn build
 cp -R img *.png *.xml *.ico REVISION.json manifest.json _redirects _headers public/
