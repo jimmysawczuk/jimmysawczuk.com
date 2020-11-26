9f40dc05ee3fd2a9fc2bba1eb2227cc30b8204b8
Jimmy Sawczuk <jsawczuk@redventures.com>
2020-11-25 19:27:51 -0500

    debug

--- netlify.sh
+++ netlify.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo $PATH
+
 wget https://github.com/jimmysawczuk/tmpl/releases/download/v2.0.0/tmpl-v2.0.0-linux-amd64
 mv tmpl-v2.0.0-linux-amd64 /usr/local/bin/tmpl
 chmod +x /usr/local/bin/tmpl
