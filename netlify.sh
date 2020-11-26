e548ef03773559f385b48ccb7c365938c7a388e4
Jimmy Sawczuk <jsawczuk@redventures.com>
2020-11-25 19:31:03 -0500

    WIP

--- netlify.sh
+++ netlify.sh
@@ -2,17 +2,16 @@
 
 set -e
 
-echo $PATH
+export PATH=$HOME/bin:$PATH
+mkdir -p $HOME/bin
 
 wget https://github.com/jimmysawczuk/tmpl/releases/download/v2.0.0/tmpl-v2.0.0-linux-amd64
-mv tmpl-v2.0.0-linux-amd64 /usr/local/bin/tmpl
-chmod +x /usr/local/bin/tmpl
+mv tmpl-v2.0.0-linux-amd64 $HOME/bin/tmpl
+chmod +x $HOME/bin/tmpl
 
 wget https://github.com/jimmysawczuk/scm-status/releases/download/v2.2.0/scm-status-v2.2.0-linux-amd64
-mv scm-status-v2.2.0-linux-amd64 /usr/local/bin/scm-status
-chmod +x /usr/local/bin/scm-status
-
-echo $PATH
+mv scm-status-v2.2.0-linux-amd64 $HOME/bin/scm-status
+chmod +x $HOME/bin/scm-status
 
 scm-status -out=REVISION.json
 yarn dark-mode
