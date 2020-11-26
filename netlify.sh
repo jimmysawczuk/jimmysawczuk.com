82f044541b0d3bbbed6909c4eb48c1d41197ece7
Jimmy Sawczuk <me@jimmysawczuk.com>
2020-11-25 19:26:21 -0500

    Fix deploy script

--- netlify.sh
+++ netlify.sh
@@ -3,14 +3,14 @@
 set -e
 
 wget https://github.com/jimmysawczuk/tmpl/releases/download/v2.0.0/tmpl-v2.0.0-linux-amd64
-mv tmpl-v2.0.0-linux-amd64 $GOPATH/bin/tmpl
-chmod +x $GOPATH/bin/tmpl
+mv tmpl-v2.0.0-linux-amd64 /usr/local/bin/tmpl
+chmod +x /usr/local/bin/tmpl
 
 wget https://github.com/jimmysawczuk/scm-status/releases/download/v2.2.0/scm-status-v2.2.0-linux-amd64
-mv scm-status-v2.2.0-linux-amd64 $GOPATH/bin/scm-status
-chmod +x $GOPATH/bin/scm-status
+mv scm-status-v2.2.0-linux-amd64 /usr/local/bin/scm-status
+chmod +x /usr/local/bin/scm-status
 
-export PATH=$GOPATH/bin:$PATH
+echo $PATH
 
 scm-status -out=REVISION.json
 yarn dark-mode
