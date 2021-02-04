e06e064d084af152fb64318856dee21aad3c562d
Jimmy Sawczuk <me@jimmysawczuk.com>
2021-02-04 16:14:07 -0500

    Upgrade tmpl version

--- netlify.sh
+++ netlify.sh
@@ -5,8 +5,8 @@ set -e
 export PATH=$HOME/bin:$PATH
 mkdir -p $HOME/bin
 
-wget https://github.com/jimmysawczuk/tmpl/releases/download/v2.0.0/tmpl-v2.0.0-linux-amd64
-mv tmpl-v2.0.0-linux-amd64 $HOME/bin/tmpl
+wget https://github.com/jimmysawczuk/tmpl/releases/download/v2.1.0/tmpl-v2.1.0-linux-amd64
+mv tmpl-v2.1.0-linux-amd64 $HOME/bin/tmpl
 chmod +x $HOME/bin/tmpl
 
 wget https://github.com/jimmysawczuk/scm-status/releases/download/v2.2.0/scm-status-v2.2.0-linux-amd64
